3995175bfcdfe6ef68d303fc8da2d257330af94d
Adeptix <hlvbnhbq@yandex.ru>
2019-10-17 20:59:55 +0300

    final

--- app/src/main/java/bonch/dev/school/FragmentActivity.kt
+++ app/src/main/java/bonch/dev/school/FragmentActivity.kt
@@ -2,15 +2,26 @@ package bonch.dev.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+
 
 class FragmentActivity : AppCompatActivity() {
+
+    private val fm = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button : Button = Button(this)
-        val b : Boolean = button.isEnabled
+        fm.beginTransaction()
+            .add(R.id.fragment1, FirstFragment())
+            .commit()
+
+    }
+
+    fun replaceFragment(){
+        fm.beginTransaction()
+            .replace(R.id.fragment1, SecondFragment())
+            .addToBackStack("second_fragment")
+            .commit()
     }
 }
