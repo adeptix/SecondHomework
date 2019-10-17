b8ad48987f6fc1aecf7c689730e7775081e2e7a0
Adeptix <hlvbnhbq@yandex.ru>
2019-10-17 10:59:44 +0300

    s commit

--- app/src/main/java/bonch/dev/school/MainActivity.kt
+++ app/src/main/java/bonch/dev/school/MainActivity.kt
@@ -2,11 +2,15 @@ package bonch.dev.school
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button : Button = Button(this)
+        val b : Boolean = button.isEnabled
     }
 }
